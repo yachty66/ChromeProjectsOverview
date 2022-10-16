a44fc382107d95222acd94a131fd143aa0fc4de0
Max Hager <maxhager28@gmail.com>
2022-10-16 15:46:42 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ChromeProjectsOverview-
+
+## What
+
+Displays all of my current projects in a new tab in GChrome. I scrape the projects from [my projects side](https://www.maxhager.xyz/).
+
+## Why
+
+This should remember all the time on which projects I am working on so that I am more focused on them.
--- createPage.py
+++ createPage.py
@@ -1,22 +1,50 @@
 from bs4 import BeautifulSoup
-import requests 
+import requests
+import re
 
-#go to https://www.maxhager.xyz/ and get every tag between first h2 tags 
+
 def getTags():
-    #soup = BeautifulSoup(html_doc, 'html.parser')
-    #go to https://www.maxhager.xyz/ and get every tag between first h2 tags with beaitiful soup
     url = "https://www.maxhager.xyz/"
     name = requests.get(url)
-    soup = BeautifulSoup(name.content) 
-    print(soup.prettify())
-    
-    #get everything from <a href="#currently-doing"> to  <a href="#finished"> from soup
-    
-    pass
+    soup = BeautifulSoup(name.content)
+    startWithAddingElemToList = False
+    domElem = []
+    h3Elem = []
+    finalElem = []
+    for elem in soup.find_all():
+        elem = str(elem)
+        if elem == '<h2 id="currently-doing">Currently doing</h2>':
+            startWithAddingElemToList = True
+        if elem == '<h2 id="finished">Finished</h2>':
+            break
+        elif startWithAddingElemToList == True:
+            domElem.append(elem)
+    for elem in domElem:
+        if elem[:3] == "<h3":
+            h3Elem.append(elem)
+    for i in h3Elem:
+        x = re.findall('id=".*"', i)
+        finalElem.append(x)
+    for i in finalElem:
+        i[0] = i[0][4:-1]
+    finalElem = finalElem[1:]
+    return finalElem
 
-#add all tags as elements in a table in index.html -- centered and visible
-def addToPage():
-    pass
+
+def addToPage(elem):
+    html = """
+    <head>
+    <title>New Tab</title>
+    </head>
+    <ul style="list-style-type:none; font-size:300%; text-align:center; margin:0; padding:0; position:absolute; top:50%; left:50%; transform:translate(-50%, -50%);">
+    das ist ein  test
+    """
+    for i in elem:
+        html = html + "<li>" + i[0] + "</li>\n"
+    with open("/Users/maxhager/Projects2022/ChromeProjectsOverview/index.html", "w") as file:
+        file.write(html)
+
 
 if __name__ == "__main__":
-    getTags()
+    tags = getTags()
+    addToPage(tags)
